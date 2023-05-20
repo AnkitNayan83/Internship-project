e952bd672d7ae7e88c3a296c4c5ab0c6101ae115
Ankit Nayan <ankitnayan83@gmail.com>
2023-05-20 16:26:02 +0530

    commit

---
+++ client/README.md
@@ -0,0 +1,6 @@
+# Created Task Screens with ReactJs as frontend and Nodejs as backend
+
+
+
+https://github.com/bhavisshyya/Kirnvi_tech---Frontend/assets/105254969/6ed05661-6c61-4157-b7ee-a397482f7504
+
---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateProfile from "./pages/createProfile/CreateProfile";
+import Profile from "./pages/profile/Profile";
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CreateProfile />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
